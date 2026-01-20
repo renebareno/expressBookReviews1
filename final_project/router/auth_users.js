d1a91c7bd654577ef65236981076186bc7e82c0d
Rene <rene.bareno@gmail.com>
2026-01-20 11:08:04 -0500

    task7

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,24 +5,43 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+const isValid = (username) => { //returns boolean
+    //write code to check is the username is valid
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const authenticatedUser = (username, password) => { //returns boolean
+    //write code to check if username and password match the one we have in records.
 }
 
 //only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(200).json({message: "Yet to be implemented"});
+regd_users.post("/login", (req, res) => {
+
+    const { username, password } = req.query
+
+    if (!username || !password) {
+        return res.status(404).json({ message: "no password or login" });
+    }
+
+    console.log(users.filter((user) => (user.username === username && user.password === password)).length)
+
+    if (users.filter((user) => (user.username === username && user.password === password)).length){
+        let accessToken = jwt.sign({
+            data: password
+        }, 'access', { expiresIn: 60 * 60 });
+
+        req.session.authorization = {
+            accessToken, username
+        }
+        return res.status(200).send("User successfully logged in");
+    } else {
+        return res.status(208).json({ message: "Invalid Login. Check username and password" });
+    }
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    //Write your code here
+    return res.status(300).json({ message: "Yet to be implemented" });
 });
 
 module.exports.authenticated = regd_users;
